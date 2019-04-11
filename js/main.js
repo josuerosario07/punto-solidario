cc6af09bb07d99c282398cdec807f3e7a4df567c
Solvex Administrator <sxadmin@SolvexMac.local>
2019-04-11 16:52:16 -0400

    mapa de sitio

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        items: 3,
+        autoplay:false,
+        autoplayTimeout:2000,
+         autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+  });
